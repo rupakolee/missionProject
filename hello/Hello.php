9184eb88ac0995525c87f786aa7982a04ba0eb09
rupakolee <rupakolee@gmail.com>
2023-08-05 20:49:10 +0545

    commit

---
+++ hello/Hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Content management system</title>
+    <link rel="stylesheet" href="Hello.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="btn-animation"> <button>SAY HELLO</button> <img src=https://drive.google.com/uc?export=view&id=111w-Qm55RJpRJEIm4Yc6V8nee1yvnJFx id="image" /> </div>
+        <button class="btn">Login</button>
+    </div>
+    <script src="Hello.js"></script>
+</body>
+
+</html>
